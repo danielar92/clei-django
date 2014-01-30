57389925f8ac6227e837d8fd6e0f43a97b3d5e88
Esteban <esteban@esteban-VirtualBox.(none)>
2014-01-30 16:21:33 -0430

    Eventos y Lugares

---
+++ evento/forms.py
@@ -0,0 +1,89 @@
+#encoding:utf-8
+from django.forms import ModelForm
+from django import forms
+from evento.models import Lugar, Evento, Charla, Ponencia
+
+class LugarForm(ModelForm):
+	class Meta:
+		model = Lugar
+
+class CharlaForm(ModelForm):
+	class Meta:
+		model = Charla
+		exclude = ('tipo',)
+		
+		
+	def clean_horaFin(self):
+		ini = self.cleaned_data['horaIni']
+		fin = self.cleaned_data['horaFin']
+		if (fin <= ini):
+			raise forms.ValidationError("Como termina antes de que empieza?!")
+			
+		return fin
+		
+class PonenciaForm(ModelForm):		
+	class Meta:
+		model = Ponencia
+		exclude = ('tipo',)
+
+	def clean_horaFin(self):
+		ini = self.cleaned_data['horaIni']
+		fin = self.cleaned_data['horaFin']
+		if (fin <= ini):
+			raise forms.ValidationError("Como termina antes de que empieza?!")
+			
+		return fin
+		
+class TallerForm(ModelForm):
+	class Meta:
+		model = Evento
+		exclude = ('tipo',)
+
+	def clean_horaFin(self):
+		ini = self.cleaned_data['horaIni']
+		fin = self.cleaned_data['horaFin']
+		if (fin <= ini):
+			raise forms.ValidationError("Como termina antes de que empieza?!")
+			
+		return fin
+
+		
+class AperturaForm(ModelForm):
+	class Meta:
+		model = Evento
+		exclude = ('tipo',)
+
+	def clean_horaFin(self):
+		ini = self.cleaned_data['horaIni']
+		fin = self.cleaned_data['horaFin']
+		if (fin <= ini):
+			raise forms.ValidationError("Como termina antes de que empieza?!")
+			
+		return fin
+		
+class ClausuraForm(ModelForm):
+	class Meta:
+		model = Evento
+		exclude = ('tipo',)
+
+	def clean_horaFin(self):
+		ini = self.cleaned_data['horaIni']
+		fin = self.cleaned_data['horaFin']
+		if (fin <= ini):
+			raise forms.ValidationError("Como termina antes de que empieza?!")
+			
+		return fin
+		
+class EventoSocialForm(ModelForm):
+	class Meta:
+		model = Evento
+		exclude = ('tipo',)
+		
+	def clean_horaFin(self):
+		ini = self.cleaned_data['horaIni']
+		fin = self.cleaned_data['horaFin']
+		if (fin <= ini):
+			raise forms.ValidationError("Como termina antes de que empieza?!")
+			
+		return fin
+		
